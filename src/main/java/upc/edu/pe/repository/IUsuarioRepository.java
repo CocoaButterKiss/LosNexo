2040b3ff4e5d0bbb0ed118f8edeb87bf3b7713fc
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-10-27 22:09:02 -0500

    Update IUsuarioRepository.java

--- src/main/java/upc/edu/pe/repository/IUsuarioRepository.java
+++ src/main/java/upc/edu/pe/repository/IUsuarioRepository.java
@@ -1,9 +1,15 @@
 package upc.edu.pe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import upc.edu.pe.entities.Usuario;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+	
+	@Query("select count(u.nombreUsuario) from Usuario u where u.nombreUsuario=:name")
+	public int usuariosExistentes(@Param("name")  String nombre);
+	
         }
