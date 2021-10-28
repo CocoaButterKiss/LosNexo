e891a02bd1a7a75269629f00b6f5b87ee66b17e3
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-10-27 22:08:13 -0500

    Update IUsuarioServiceImpl.java

--- src/main/java/upc/edu/pe/serviceimpl/IUsuarioServiceImpl.java
+++ src/main/java/upc/edu/pe/serviceimpl/IUsuarioServiceImpl.java
@@ -1,54 +1,34 @@
 package upc.edu.pe.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import upc.edu.pe.entities.Usuario;
-import upc.edu.pe.repository.INotificacionRepository;
 import upc.edu.pe.repository.IUsuarioRepository;
 import upc.edu.pe.serviceinterface.IUsuarioService;
 
-import java.util.List;
-
 public class IUsuarioServiceImpl implements IUsuarioService {
 
-    private final IUsuarioRepository usuarioRepository;
-
-
-    @Autowired
-    public IUsuarioServiceImpl(IUsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
+	@Autowired
+    private  IUsuarioRepository uR;
 
 
     @Override
-    public boolean insert(Usuario usuario) {
-        try {
-            usuarioRepository.save(usuario);
-        } catch (Exception e) {
-            System.out.println(e);
-            return false;
-        }
-        return true;    }
+    public Integer insert(Usuario usuario) {
+    	int rpta = uR.usuariosExistentes(usuario.getNombreUsuario());
+		if (rpta == 0) {
+			uR.save(usuario);
+		}
+		return rpta;
+		}
+    
 
     @Override
-    public List<Usuario> list() {
-        return usuarioRepository.findAll();
-    }
+	public List<Usuario> list() {
+		// TODO Auto-generated method stub
+		return uR.findAll();
+	}
 
-    @Override
-    public void eliminar(int idUsuario) {
-        usuarioRepository.deleteById(idUsuario);
-
-    }
-
-    @Override
-    public boolean modificar(Usuario usuario) {
-        boolean flag = false;
-        try {
-            usuarioRepository.save(usuario);
-            flag = true;
-        } catch (Exception ex) {
-            System.out.println("Sucedio un roche...");
-        }
-        return flag;
-    }
+   
 }
