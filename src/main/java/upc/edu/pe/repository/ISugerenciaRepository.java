7b9e9cb282a147b4411b8d5b0322287ccbf952bb
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-10-27 22:09:55 -0500

    Update ISugerenciaRepository.java

--- src/main/java/upc/edu/pe/repository/ISugerenciaRepository.java
+++ src/main/java/upc/edu/pe/repository/ISugerenciaRepository.java
@@ -1,9 +1,14 @@
 package upc.edu.pe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import upc.edu.pe.entities.Sugerencia;
+import upc.edu.pe.entities.Suscripcion;
 
 @Repository
-public interface ISugerenciaRepository extends JpaRepository<Sugerencia, Integer> {
+public interface ISuscripcionRepository extends JpaRepository<Suscripcion, Integer> {
+	@Query("select count(l.) from Suscripcion l where l.usuario=:name")
+	public int buscarSuscripcion(@Param("name") String nombre);
+	
 }
