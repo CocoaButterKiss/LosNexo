15e93a14de03bdb5d8094348e49e1856b4001e3b
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-10-27 22:07:46 -0500

    Update ISuscripcionService.java

--- src/main/java/upc/edu/pe/serviceinterface/ISuscripcionService.java
+++ src/main/java/upc/edu/pe/serviceinterface/ISuscripcionService.java
@@ -1,14 +1,17 @@
 package upc.edu.pe.serviceinterface;
 
-import upc.edu.pe.entities.Categoria;
+
 import upc.edu.pe.entities.Suscripcion;
 
 import java.util.List;
 
+
+
 public interface ISuscripcionService {
-    public boolean insert(Suscripcion suscripcion);
-    public void eliminar(int idSuscripcion);
-    boolean modificar(Suscripcion suscripcion);
-    List<Suscripcion> list();
+	public boolean insert(Suscripcion suscripcion);
+
+	List<Suscripcion> list();
+
+	Suscripcion listarId(int idSuscripcion);
 }
 
